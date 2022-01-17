e064c209dd61d65164de1b8c0c4d3476e1a073d7
Marcin T <yahoospider@gmail.com>
2022-01-17 10:54:45 +0100

    sys argv

--- accountant.py
+++ accountant.py
@@ -7,14 +7,14 @@ f) python accountant.py przegląd"""
 
 import sys
 
-akcja= ["saldo", "sprzedaz", "zakup", "konto", "magazyn", "przeglad", "stop"]
+akcja= ["saldo", "zakup", "sprzedaz", "konto", "magazyn", "przeglad", "stop"]
 saldo = 0
 sprzedaz = 0
 zakup = 0
 konto = ""          # sprawdzic - po co to definiowac tu?
 magazyn = {}        # slownik
 historia = []       # lista wyswietlana w przeglad
-stop = True
+stop = True         # konczy program, ewnetualnie dodac wszedzie
 komentarz = True    # przydatne do anulowania transakcji na poziomie komentarza w saldzie
 wartosc = 0         # poczatkowe saldo na koncie
 stan_konta = 0      # tu przechowuje aktualne saldo
@@ -23,38 +23,37 @@ wartosc_produkt = 0 # wartosc kupionego produktu (cena * ilosc)
 
 
 while True:
-    print('#' * 87)
-    print(f'Dostepne akcje: {akcja}\nCo wybierasz?')  # jak w 1 lini te 3 printy?
-    print('#' * 87)
+# jak w 1 lini te 3 printy?
+#print('#' * 87)
+#print(f'Dostepne akcje: {akcja}\nCo wybierasz?')
+#print('#' * 87)
 
-    wybor = str(input())
-
+    wybor = str(input("co wybierasz?"))
     if wybor not in akcja:
         print("Zły wybor. Jeszcze raz")
         continue
 
     if wybor == "saldo":
-        print('/jestesmy w saldo/')
-        print('podaj wartosc transakcji')
+        wartosc = int(input('podaj wartosc transakcji'))
 
-        wartosc = int(input())
+        if wartosc == 0:
+            print('Wpłać ciut więcej ;)')
+            continue
 
-
-# or stan_konta > wartosc:
-        if wartosc <= 0 and stan_konta == 0:
+        if stan_konta + wartosc <= 0:
             print('Zla kwota, brak mozliwosci wyplaty')
             continue
 
-        print('podaj komentarz do transakcji - (STOP=anulowanie transakcji)')
-        komentarz = str(input())
-
+        komentarz = str(input('podaj komentarz do transakcji - (STOP=anulowanie transakcji)'))
         if komentarz == "stop":
             print("Transakcja anulowana")
             continue
 
         stan_konta += wartosc
-        historia_saldo = (wartosc, komentarz)
+        historia_saldo = (wybor,wartosc,komentarz)
         historia.append(historia_saldo)
+
+        print(f"Saldo: {wartosc} {komentarz}")
         continue
 
     if wybor == "zakup":
@@ -84,51 +83,36 @@ while True:
         stan_konta -= cena * ilosc
         wartosc_produkt = cena * ilosc
 
-# zle oblicza wartośc w przegladzie jesli wczesniej były w saldo  minusy (wyplaty)
-# rozdzielic na wartosc_zakup oraz wartosc_sprzedaz ???
-
-
-
-# printa ponizej poprawic
-#        print(f,"Kupiono",{sztuk}, {produkt}, "o wartosci:", {wartosc_produkt})
+        print(f"Kupiono,{ilosc}, {produkt}, o wartosci:, {wartosc_produkt}")
 
         if produkt in magazyn:
             magazyn[produkt] += ilosc
         else:
             magazyn[produkt] = ilosc
 
-        historia_zakup = (produkt,cena,ilosc, wartosc)
+        historia_zakup = (wybor,produkt,cena,ilosc)
         historia.append(historia_zakup)
 
         continue
 
-
     if wybor == "sprzedaz":
-        print('/jestesmy w sprzedaz/')
-
-
         print('podaj nazwe produktu do sprzedania:')
         produkt = str(input())
 
-#  poprawic sprawdzenie czy jest w magazynie (not in magazyn) !!!!
-
-# if produkt not in magazyn:
-# print('Brak produktu w magazynie!')
-# continue
-
+        if produkt not in magazyn:
+            print('Brak produktu w magazynie!')
+            continue
         print('podaj cene sprzedazy produktu:')
         cena = int(input())
         if cena <= 0:
             print('Zla cena!')
             continue
-
         print('podaj ilosc sztuk do sprzedania:')
         ilosc = int(input())
         if ilosc <= 0:
             print('Zła ilość!')
             continue
-
-        if ilosc < ilosc in magazyn:
+        if magazyn[produkt] < ilosc:
             print('Brak takiej ilosci w magazynie')
             continue
 
@@ -136,53 +120,93 @@ while True:
         stan_konta += cena * ilosc
         magazyn[produkt] -= ilosc
 
-# do zrobienia: wyrzucic produkt z magazynu gdy stan produktu = zero ????
-
-        historia_sprzedaz = (produkt,cena,ilosc, wartosc)
+        historia_sprzedaz = (wybor,produkt,cena,ilosc)
         historia.append(historia_sprzedaz)
 
-# printa ponizej poprawic
-#        print(f,"Sprzedano",{ilosc}, {produkt}, "o wartosci:", {wartosc_produkt})
+        print(f"Sprzedano,{ilosc}, {produkt}, o wartosci:, {wartosc_produkt}")
         continue
 
     if wybor == "konto":
-        print('/jestesmy w  konto/')
         print (f'aktualny stan konta to: {stan_konta}')
         continue
 
     if wybor == "magazyn":
-        print('/jestesmy w  magazyn/')
 
-# zrobic: w nowych liniach, bez nawiasów
-
+# stany magazynowe dla podanych produktów, w formacie:
+# <id produktu>: <stan> w nowych liniach i kończy działanie:
         print (f'aktualny stan magazynu to:\n {magazyn}')
+
+
         continue
 
-# program wypisuje stany magazynowe dla podanych produktów, w formacie:
-# <id produktu>: <stan> w nowych liniach i kończy działanie:
+
 
     if wybor == "przeglad":
         print('/jestesmy w  przeglad/')
-        print(historia)
+#       print(historia)
+        for ile in historia:
+            print(ile)
+# wypisuje wszystkie akcje zapisane pod indeksami w zakresie [od, do] (zakresy włącznie)
+
+
         continue
 
+
+
     if wybor == "stop":
-        print('jestesmy w stop')
-        print('koniec')
+        print('STOP')
         break
 
-"""
+# argv
 
-sad = sys.argv[1]        # argv pod saldo
-spr = sys.argv[1]        # argv pod sprzedaz
-zak = sys.argv[1]        # argv pod zakup
+if sys.argv[1] == "saldo":
+    wartosc = int(sys.argv[2])
+    komentarz = sys.argv[3]
+    stan_konta += wartosc
+    historia_saldo = (wybor, wartosc, komentarz)
+    historia.append(historia_saldo)
 
-kon= sys.argv[1]        # argv pod konto
-len(sys.argv))
-for x in range
-
-mga = sys.argv[1]        # argv pod magazyn
-prz = sys.argv[1]        # argv pod przeglad
+    print(f"{sys.argv[1]}\n{wartosc}\n{komentarz}")
 
 
-"""
+if sys.argv[1] == "zakup":
+    produkt = str(sys.argv[2])
+    cena = int(sys.argv[3])
+    ilosc = int(sys.argv[4])
+
+    stan_konta -= cena * ilosc
+    wartosc_produkt = cena * ilosc
+
+
+    historia_zakup = (wybor,produkt,cena,ilosc)
+    historia.append(historia_zakup)
+
+    print(f"{sys.argv[1]}\n{produkt}\n{cena}\n{ilosc}")
+
+if sys.argv[1] == "sprzedaz":
+    produkt = str(sys.argv[2])
+    cena = int(sys.argv[3])
+    ilosc = int(sys.argv[4])
+
+    wartosc_produkt = cena * ilosc
+    stan_konta += cena * ilosc
+    magazyn[produkt] -= ilosc
+
+    historia_sprzedaz = (wybor,produkt,cena,ilosc)
+    historia.append(historia_sprzedaz)
+
+    print(f"{sys.argv[1]}\n {ilosc}\n{produkt},\n{cena}")
+
+if sys.argv[1] == "konto":
+    print(f'{sys.argv[1]}\n {stan_konta}')
+
+
+if sys.argv[1] == "magazyn":
+    print (f'{sys.argv[1]}\n {magazyn}')
+
+# len(sys.argv))
+# for x in range
+# sys.argv[1] ==
+
+if wybor == "przeglad":
+    print(f'{sys.argv[1]}\n {historia}')
